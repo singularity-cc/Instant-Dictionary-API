1b9440585695b7b00ad42a355caec024f61d433b
hanyuzhang <zhhyok@gmail.com>
2021-12-27 22:04:59 -0500

    Change to json output and add documentation.py

--- api.py
+++ api.py
@@ -1,16 +1,24 @@
 import justpy as jp
+import definition
+import json
 
 
 class Api:
-
+    """Handle requests by /api?w=word
+    """
     @classmethod
     def serve(cls, req):
         wp = jp.WebPage()
         word = req.query_params.get('w')
-        jp.Div(a=wp, text=word.title())
+        defined = definition.Definition(word).get()
+
+        response = {
+            "word": word,
+            "definition": defined
+        }
+        wp.html = json.dumps(response)
         return wp
 
-jp.Route("/", Api.serve)
-jp.justpy()
+
 
 
